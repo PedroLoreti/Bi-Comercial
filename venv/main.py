ec5c4cfd901dd6e3138bbeed2ff8b091439ca487
Loreti <111466632+PedroLoreti@users.noreply.github.com>
2025-08-18 13:38:49 -0300

    Create main.py

---
+++ venv/main.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+try:
+    connect = psycopg2.connect(
+        database="CPlus5",
+        user="consulta",
+        password="vvs@2025@",
+        host="192.168.10.225",
+        port="5432"
+    )
+    print("Conexão estabelecida com sucesso!")
+
+except psycopg2.Error as e:
+    print("Erro ao conectar ao banco de dados", e)
+
+
+cur = connect.cursor()
+
+
+cur.execute("SELECT * FROM dbo.bi_fato WHERE datafaturamento = '2025-08-14';")
+
+rows = cur.fetchall()
+
+for row in rows[:100]:
+    print(row)
